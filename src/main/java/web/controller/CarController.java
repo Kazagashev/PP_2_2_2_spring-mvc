7056c3612c358375cb324d7363b31cb4bbdeaa91
Kazagashev <gimbarrabakan@gmail.com>
2023-09-14 15:17:53 +0700

    2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDaoImp;
 import web.model.Car;
+import web.service.CarServiceImp;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +15,7 @@ public class CarController {
     @GetMapping("/cars")
     public String printCar(@RequestParam(defaultValue = ("5"),value = "count", required = false) Integer count, ModelMap model) {
 
-        List<Car> cars = new CarDaoImp().cars(count);
+        List<Car> cars = new CarServiceImp().cars(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -2,16 +2,19 @@ package web.service;
 
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarServiceImp implements CarService{
-    @Override
-    public List<Car> getCars() {
-    return null;
-    }
 
     @Override
-    public int count() {
-        return 0;
+    public List<Car> cars(int count) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Toyota", "Camry", 2022));
+        cars.add(new Car("Honda", "Stream", 2023));
+        cars.add(new Car("Nissan", "Primera", 2024));
+        cars.add(new Car("Hyundai", "Solaris", 2025));
+        cars.add(new Car("Toyota", "LandCruiser", 2026));
+        return cars.subList(0, count);
     }
 }
